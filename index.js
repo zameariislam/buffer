d8e1184a13e91e9cc77b8394fd20b1838e03d912
Zameari <[zameariislam@gmail.com]>
2025-07-15 14:35:11 +0200

    practice buffer and stream together

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-console.log('hello')
+const fs=require('fs');
+
+const readStream= fs.createReadStream(`${__dirname}/text.txt`);
+const writeStream= fs.createWriteStream(`${__dirname}/out.txt`);
+
+
+ const data= readStream.on('data',(chunk)=>{
+
+writeStream.write(chunk)
+
+
+
+})
+
+const buf = Buffer.from([104, 101, 108, 108, 111])
+
+
+
+const str = "Hello,xx world!";
+const base64 = Buffer.from(str).toString('utf-8');
+
+console.log(base64); // "SGVsbG8sIHdvcmxkIQ=="
